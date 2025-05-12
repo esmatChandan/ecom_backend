2378bd7aac33e0045ca2b63b57da135f72c1dd67
Chandan Kumar Shaw <147383845+esmartchandan@users.noreply.github.com>
2025-05-12 21:07:29 +0530

    shotout some err

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ app.use(helmet());
 
 // CORS middleware
 app.use(cors({
-  origin: '*', // Change to ['http://localhost:3000', 'https://yourdomain.com'] in production
+  origin: 'https://ecom-backend-d27a.onrender.com/',
+ origin: 'https://desitasty.com', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
